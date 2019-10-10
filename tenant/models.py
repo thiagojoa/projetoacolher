25c668087348154ef7a0e7d87b1441ab838d5de5
Ricardo Gaya <rrgaya@gmail.com>
2019-10-09 23:54:37 -0300

    Adicionando modelagem de tenant's #3

--- tenant/models.py
+++ tenant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Tenant(models.Model):
+    """"""
+    name = models.CharField(max_length=100)
+    subdomain_prefix = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return f"name: {self.name} - subdomain-prefix: {self.subdomain_prefix}"
+
+
+
+class TenantAwareModel(models.Model):
+    """"""
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    class Meta:
+        abstract = True
