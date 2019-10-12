5a41d14970c888cb085c6af0da6069a56a36fc32
Ricardo Gaya <rrgaya@gmail.com>
2019-10-11 22:47:21 -0300

    Adicionado ongs e tenant na modelagem #3

--- tenant/models.py
+++ tenant/models.py
@@ -1,8 +1,20 @@
 from django.db import models
 
+CARGO_DIRIGENTE = (
+    ('DIR', 'Dirigente'),
+    ('VOL', 'Voluntário'),
+    ('FRE', 'Freelancer'),
+)
+
+
+CAUSAS = (
+    ("Social", "Causas Sociais"),
+)
+
 
 class Tenant(models.Model):
-    """"""
+    """Esta classe modela a multitenancy adicionando subdomínios como identificador
+    """
     name = models.CharField(max_length=100)
     subdomain_prefix = models.CharField(max_length=100, unique=True)
 
@@ -10,12 +22,50 @@ class Tenant(models.Model):
         return f"name: {self.name} - subdomain-prefix: {self.subdomain_prefix}"
 
 
-
 class TenantAwareModel(models.Model):
-    """"""
+    """Esta classe modela a multitenancy
+    """
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
     class Meta:
         abstract = True
+
+
+class Ong(TenantAwareModel):
+    """Esta classe modela as Ong's
+    """
+    nome = models.CharField(max_length=200)
+    resumo = models.TextField()
+    imagem = models.ImageField()
+    endereco = models.CharField(max_length=200)
+    complemento = models.CharField(max_length=200)
+    causas = models.CharField(max_length=10, choices=CAUSAS, default=None)
+    numero_beneficiaros = models.IntegerField()
+    cnpj = models.CharField(unique=True, max_length=16, blank=True, null=True)
+    is_active = models.BooleanField(default=True, null=False)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.CharField(max_length=50, blank=True, null=True)
+    website = models.URLField()
+    facebook_ong = models.URLField()
+    descricao = models.TextField()
+
+    class Meta:
+        verbose_name = "Ong"
+        verbose_name_plural = "Ong's"
+
+    def __str__(self):
+        return self.nome
+
+
+class Dirigente(TenantAwareModel):
+    """Esta classe modela os dirigentes
+    """
+    cargo = models.CharField(max_length=3, choices=CARGO_DIRIGENTE, default=None)
+
+
+class Fornecedor(TenantAwareModel):
+    """Esta classe modela os fonercedores
+    """
+    pass
