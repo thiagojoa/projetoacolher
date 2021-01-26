02bb78037010c94b44b4c6f41bf1be36386eefb5
Thiago Martins Silveira Bueno <36665874+thiagojoa@users.noreply.github.com>
2021-01-26 08:20:19 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -16,8 +16,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Allow all host headers
-#ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-ALLOWED_HOSTS= ['acolherjoanopolis.org.br']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+#ALLOWED_HOSTS= ['acolherjoanopolis.org.br']
 
 # Application definition
 
